e82908df95fa9bc1d043b45e315a5949d7401947
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2024-01-11 13:42:59 +0100

    Debugging

--- frontend/src/components/common/StaticInput.jsx
+++ frontend/src/components/common/StaticInput.jsx
@@ -7,7 +7,7 @@ function TextInput(props) {
     <div className={`inputBox ${props.marginTop ? "" : "noMargin"}`}>
       <input
         placeholder={props.placeholder}
-        className={`placeholder-dark-gray-color-600 font-bold`}
+        className={`placeholder-dark-gray-color-600 importantFontBold`}
         readOnly 
       />
       <span>{props.value}</span>
